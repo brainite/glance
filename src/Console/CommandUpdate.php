f96d8b04bbdcba4b3f081d77c957ffa23f73f904
Greg Payne <greg.payne@gmail.com>
2015-02-19 10:05:07 -0600

    Closes #11 - Load all issues by main filter and then handle common basic filters without further requests.

--- src/Console/CommandUpdate.php
+++ src/Console/CommandUpdate.php
@@ -16,6 +16,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Yaml\Yaml;
 
 class CommandUpdate extends \Symfony\Component\Console\Command\Command {
+  protected $client;
+
   protected function configure() {
     $this->setName('glance:update');
     $this->setDescription('Update the reports based on the target configuration');
@@ -76,7 +78,8 @@ class CommandUpdate extends \Symfony\Component\Console\Command\Command {
       //       $client = new \Github\Client(new \Github\HttpClient\CachedHttpClient(array(
       //         'cache_dir' => '/tmp/github-api-cache-' . get_current_user(),
       //       )));
-      $client = new \Github\Client();
+      $this->client = new \Github\Client();
+      $client = &$this->client;
       if (!empty($token_override)) {
         $output->writeln("Authenticating via token");
         $client->authenticate($token_override, \Github\Client::AUTH_HTTP_TOKEN);
@@ -94,20 +97,53 @@ class CommandUpdate extends \Symfony\Component\Console\Command\Command {
         $conf = array_merge($defaults, (array) $conf);
         foreach ($conf['repos'] as $repo) {
           list($search_user, $search_repo) = explode('/', $repo, 2);
-          foreach ($conf['weights'] as $weight) {
-            // Get the issues for each weight/filter.
-            $filter = "$conf[filter] $weight[filter]";
-            $filter = strtr($filter, $tr);
-            $results = $client->api('search')->issues("repo:$repo $filter");
-            $output->writeln(sizeof($results['items'])
-              . " results for '$filter'");
-            foreach ($results['items'] as $result) {
-              $id = $result['html_url'];
 
-              if (!isset($issues[$id])) {
-                $issues[$id] = $result;
-                $weights[$id] = 1;
+          // Get a list of all issues.
+          $items = $this->getItems($conf['filter'], $repo);
+          foreach ($items as $item) {
+            $id = $item['html_url'];
+            $issues[$id] = $item;
+            $weights[$id] = 1;
+          }
+
+          // Process the weights.
+          foreach ($conf['weights'] as $weight) {
+            $filter = trim(strtr($weight['filter'], $tr));
+
+            // Process the filters
+            if (preg_match('@label:("[^"]+"|[^ ]+)$@s', $filter, $arr)) {
+              // Handle a basic label filter
+              $items = array();
+              foreach ($issues as $issue) {
+                foreach ($issue['labels'] as $label) {
+                  if (strcasecmp($arr[1], $label['name']) === 0) {
+                    $items[] = $issue;
+                    break;
+                  }
+                }
               }
+              $output->writeln(sizeof($items) . " results for basic label '$filter'");
+            }
+            elseif (preg_match('@milestone:("[^"]+"|[^ ]+)$@s', $filter, $arr)) {
+              // Handle a basic milestone filter
+              $items = array();
+              foreach ($issues as $issue) {
+                if (isset($issue['milestone']) && strcasecmp($arr[1], $issue['milestone']['title']) === 0) {
+                  $items[] = $issue;
+                }
+              }
+              $output->writeln(sizeof($items) . " results for basic milestone '$filter'");
+            }
+            else {
+              // If this is not handled internally, use the search.
+              $filter = "$conf[filter] $filter";
+              $items = $this->getItems($filter, $repo);
+              $output->writeln(sizeof($items) . " results for '$filter'");
+            }
+
+            // Apply the weighting.
+            foreach ($items as $item) {
+              $id = $item['html_url'];
               $weights[$id] *= $weight['weight'];
             }
           }
@@ -118,7 +154,7 @@ class CommandUpdate extends \Symfony\Component\Console\Command\Command {
       arsort($weights);
       $i = 0;
       foreach ($weights as $id => $weight) {
-        if ($weight <= 0) {
+        if ($weight <= 1) {
           continue;
         }
         $link = (++$i) . ". [" . $issues[$id]['title'] . "]("
@@ -150,6 +186,17 @@ class CommandUpdate extends \Symfony\Component\Console\Command\Command {
       $output->writeln("GitHub exception: " . $e->getMessage());
       return;
     }
+  }
+
+  private function getItems($filter, $repo) {
+    static $api = NULL;
+    if (!isset($api)) {
+      $api = $this->client->api('search');
+      $api->setPerPage(1000);
+    }
+
+    $results = $api->issues("repo:$repo $filter");
+    return (array) $results['items'];
   }
 
 }
